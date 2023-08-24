64ab23e0c217254b4db389acbd9d47797ad2393a
soogs <s.park_1@tilburguniversity.edu>
2023-08-24 16:35:35 +0200

    dill replaced with pickle when dumping and loading pipelines

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 import dill
+import pickle
 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -26,7 +27,7 @@ def save_object (file_path, obj):
 		os.makedirs(dir_path, exist_ok = True)
 
 		with open (file_path, "wb") as file_obj:
-			dill.dump(obj, file_obj)
+			pickle.dump(obj, file_obj)
 
 	except Exception as e:
 		raise CustomException(e, sys)
@@ -72,7 +73,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models, param):
 def load_object(file_path):
 	try:
 		with open(file_path, "rb") as file_obj:
-			return dill.load(file_obj)
+			return pickle.load(file_obj)
 
 	except Exception as e:
 		raise CustomException(e, sys)
